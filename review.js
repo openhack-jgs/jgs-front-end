6f982c1295d6def01d028b41732404cba57448e3
gudwnsgur <gudwnsgur12@naver.com>
2019-06-28 00:11:55 +0900

    modify Review

--- review.js
+++ review.js
@@ -8,18 +8,6 @@ $(function()
         });
       });
 
-      $(".declation").click(function() {
-        $("#myModal").css({
-          "display": "none"
-        });
-      });
-
-      $(".submit").click(function() {
-        $("#myModal").css({
-          "display": "none"
-        });
-      });
-
       $("html").click(function(event) {
         if (event.target.id === "myModal") {
           $("#myModal").css({
