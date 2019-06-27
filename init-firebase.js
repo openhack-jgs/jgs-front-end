e1a4776cfe82bdeda15c53660bcc360178673016
inthewalter <21400564@handong.edu>
2019-06-28 04:28:20 +0900

    temp

---
+++ init-firebase.js
@@ -0,0 +1,13 @@
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyA9YLRoZV1IbePJYXXufrKL85fN4j2PbSw",
+  authDomain: "running-practice.firebaseapp.com",
+  databaseURL: "https://running-practice.firebaseio.com",
+  projectId: "running-practice",
+  storageBucket: "running-practice.appspot.com",
+  messagingSenderId: "1058972963108",
+  appId: "1:1058972963108:web:e5f496859285edfe"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
